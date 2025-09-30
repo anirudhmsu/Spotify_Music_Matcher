bf90d94d1c6de578d6b38da96166e6964f5fd091
anirudhmsu <anirudhms247@gmail.com>
2025-09-30 18:10:42 +0530

    chore: add more models

--- app/models/user.py
+++ app/models/user.py
@@ -1,7 +1,7 @@
 # app/models/user.py
 from typing import Optional
 from sqlmodel import SQLModel, Field
-from datetime import datetime
+from datetime import datetime, UTC
 
 class User(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -9,7 +9,7 @@ class User(SQLModel, table=True):
     display_name: Optional[str] = None
     avatar_url: Optional[str] = None
     country: Optional[str] = None
-    created_at: datetime = Field(default_factory=datetime.utcnow)
+    created_at: datetime = Field(default_factory=lambda: datetime.now(UTC))
 
 class SpotifyToken(SQLModel, table=True):
     user_id: int = Field(primary_key=True, foreign_key="user.id")
